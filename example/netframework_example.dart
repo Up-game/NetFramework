8415f3c59008782d24bde4706f7a38b96b198dd7
Chloé <cfontaine@citycable.ch>
2022-09-11 20:21:34 +0200

    Start project

---
+++ example/netframework_example.dart
@@ -0,0 +1,6 @@
+import 'package:netframework/netframework.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/netframework_test.dart
@@ -0,0 +1,16 @@
+import 'package:netframework/netframework.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
