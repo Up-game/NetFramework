c822ffa1853083658b2de5454753d77dc9c61b3a
Chloé <cfontaine@citycable.ch>
2022-09-11 21:23:21 +0200

    Add message pack dep

--- test/netframework_test.dart
+++ test/netframework_test.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:netframework/netframework.dart';
 import 'package:test/test.dart';
+import 'package:messagepack/messagepack.dart';
 
 void main() {
   group('A group of tests', () {
@@ -11,6 +14,20 @@ void main() {
 
     test('First Test', () {
       expect(awesome.isAwesome, isTrue);
+    });
+
+    test('Byte read', () {
+      final p = Packer();
+      p.packString('hi');
+      final b = p.takeBytes();
+      final u = Unpacker.fromList(b);
+
+      final s = u.unpackString();
+
+      print('object');
+      print(s.toString());
+
+      expect(s, 'hi');
     });
   });
 }
