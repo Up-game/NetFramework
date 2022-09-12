4801000a565b14c62322001c7fb9ac5a80908f47
Luca <coduri.vitor@gmail.com>
2022-09-12 13:09:03 +0200

    WIP

--- lib/src/connection.dart
+++ lib/src/connection.dart
@@ -7,17 +7,23 @@ import 'message.dart';
 class Connection {
   final Socket _socket;
   bool _isOpen = true;
-  final Queue<Message> _messagesQueueIn;
+  final Queue<OwnedMessage> _messagesQueueIn;
 
   bool get isOpen => _isOpen;
 
-  Connection({required Socket socket, required Queue<Message> messagesQueueIn})
+  Connection(
+      {required Socket socket, required Queue<OwnedMessage> messagesQueueIn})
       : _socket = socket,
         _messagesQueueIn = messagesQueueIn {
     socket.listen(onEvent, onDone: onDone, onError: onError);
   }
 
-  void onEvent(Uint8List data) {}
+  void onEvent(Uint8List data) {
+    final message = Message.fromBytes(data);
+
+    final ownedMessage = OwnedMessage(connection: this, message: message);
+    _messagesQueueIn.add(ownedMessage);
+  }
 
   void onDone() {
     _isOpen = false;
