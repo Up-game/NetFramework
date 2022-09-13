7c9223473973f825d254da17fef2b3951009db98
Luca <coduri.vitor@gmail.com>
2022-09-13 18:52:43 +0200

    Comments + example

--- example/netframework_example.dart
+++ example/netframework_example.dart
@@ -1,6 +1,60 @@
 import 'package:netframework/netframework.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+class MyClient extends Client {}
+
+class MyServer extends Server {
+  MyServer(int port) : super(port);
+
+  void handleTest(Connection connection, Message message) {
+    int? i = message.getInt();
+    String? s = message.getString();
+    print("Handling message: $i, $s");
+
+    Message response = Message(header: MessageHeader(id: "response"));
+    response.addHeader();
+    response.addString(s!);
+
+    sendToClient(connection, message);
+  }
+
+  @override
+  void onMessage(Connection connection, Message message) {
+    print("Server received message: $message");
+    switch (message.header.id) {
+      case 'test':
+        handleTest(connection, message);
+    }
+  }
+}
+
+void main() async {
+  MyServer server = MyServer(6000);
+  await server.start();
+
+  MyClient client = MyClient();
+  await client.connect('localhost', 6000);
+
+  Message m = Message(header: MessageHeader(id: 'test'));
+  m.addHeader();
+  m.addInt(10);
+  m.addString('Hello world');
+
+  client.send(m);
+
+  for (int i = 0; i < 10; i++) {
+    server.update();
+
+    if (client.incoming.isNotEmpty) {
+      OwnedMessage response = client.incoming.removeFirst();
+      Message m = response.message;
+      String? s = m.getString();
+      print("Client received: $s");
+      break;
+    }
+
+    await Future.delayed(Duration(milliseconds: 500));
+  }
+  await client.disconnect();
+  await Future.delayed(Duration(milliseconds: 500));
+  await server.stop();
 }
--- lib/src/message.dart
+++ lib/src/message.dart
@@ -18,6 +18,7 @@ class Message {
       : _unpacker = unpacker,
         header = header ?? MessageHeader();
 
+  /// Creates a new Message from a [Uint8List].
   factory Message.fromBytes(Uint8List data) {
     final Unpacker unpacker = Unpacker(data);
 
@@ -28,49 +29,66 @@ class Message {
     return Message(header: header, unpacker: unpacker);
   }
 
+  /// Get the data as a [Uint8List].
+  ///
+  /// It will return null if the data is not yet packed.
   Uint8List? get data => _data;
+
+  /// Get the data size, header included.
   int get size => _data?.length ?? 0;
 
+  /// It adds the header to the message.
+  ///
+  /// It should be called before adding any data.
   void addHeader() {
     _packer.packString(header.id.toString());
   }
 
+  /// Add an [int] to the message.
   void addInt(int value) {
     _packer.packInt(value);
   }
 
+  /// Add a [String] to the message.
   void addString(String value) {
     _packer.packString(value);
   }
 
+  /// Add a [bool] to the message.
   void addBool(bool value) {
     _packer.packBool(value);
   }
 
+  /// Add a [double] to the message.
   void addDouble(double value) {
     _packer.packDouble(value);
   }
 
+  /// get an [int] from the message.
   int? getInt() {
     assert(_unpacker != null);
     return _unpacker!.unpackInt();
   }
 
+  /// get a [Double] from the message.
   double? getDouble() {
     assert(_unpacker != null);
     return _unpacker!.unpackDouble();
   }
 
+  /// get a [String] from the message.
   String? getString() {
     assert(_unpacker != null);
     return _unpacker!.unpackString();
   }
 
+  /// get a [bool] from the message.
   bool? getBool() {
     assert(_unpacker != null);
     return _unpacker!.unpackBool();
   }
 
+  /// Pack the message.
   void pack() {
     if (_data != null) return;
     _data = _packer.takeBytes();
